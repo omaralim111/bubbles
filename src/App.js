b05c7e505cb3d807b74833dbf6ba43330d12c533
COR Coding <corcoding@Coding-13.local>
2024-12-15 21:10:08 -0500

    metaverse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const loader = new GLTFLoader();
+
+    return 
+
+        <a-scene>
+            <a-assets>
+            
+            </a-assets>
+        </a-sceneO>
 }
 
 export default App;
