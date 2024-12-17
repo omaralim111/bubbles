9d1727df7dee6784a12ff4995ccbff6e0612c9c5
COR Coding <corcoding@Coding-13.local>
2024-12-16 21:57:38 -0500

    add

--- src/App.js
+++ src/App.js
@@ -4,39 +4,22 @@ import 'aframe';
 import 'GLTFLoader' from 'three/examples/jsm/loaders/GLTFLoader';
 import solar from './one.jpg'
 
-function App() 
-    const loader = new GLTFLoader();
+function App(){ 
+  const loader = new GLTFLoader();
 
-    return 
-
-        <a-scene>
-            <a-assets>
-            
-            </a-assets>
-        </a-sceneO>
-
-<<<<<<< HEAD
-=======
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <a-scene>
+      <a-assets>
+        <img id= "solar"  />
+
+      </a-assets>
+
+        <a-solar
+          color = "#FFFFFF"
+          MATERIAL="SRC: "
+        />
+    </a-scene>
+  );  
 }
->>>>>>> 1fb0a6e0d94ec382755730d49031554928e64a97
 
 export default App;
