3eef9a61f46d64bc60c7ca34f30f9f5707e4ccd9
COR Coding <corcoding@Coding-13.local>
2024-12-18 21:43:21 -0500

    dont be shy buy1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App(){
     <div>
       <header className='App-header'/> 
         <p>
-        DONT<code>BE SHY </code>BUY  
+        DONT/n<code>BE SHY </code>BUY  
         <p>      </p>
         </p>
         <image src="/one.jpg" alt=""/>
