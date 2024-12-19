b93154c103fb4b7b67d41f309edcb932fd52e397
COR Coding <corcoding@Coding-13.local>
2024-12-18 22:37:32 -0500

    ioop99

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import solar from './one.jpg';
 import beating from './Beatin on the door.mp3'
 
 function App(self){ 
-  self.play.Audio
-  let audio = new Audio("/Beatin on the door.mp3")
-  const start = {
-  }
   return (       
     <div>
       <header className='App-header'/> 
         <p>
-        DONT <code>BE SHY </code>BUY  <buttfon onClick={play}/>
+        DONT <code>BE SHY </code>BUY   
         <p>      </p>
         </p>
         <image src="/one.jpg" alt=""/>
