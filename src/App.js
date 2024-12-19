a9bb545ded30137aeb6b16e97bd8a30c4e03049c
COR Coding <corcoding@Coding-13.local>
2024-12-18 22:00:21 -0500

    dont be shy buy123

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import 'aframe';
 import solar from './one.jpg';
+import beating from './Beatin on the door.mp3'
 
 function App(){ 
+  let audio = new Audio("/Beatin on the door.mp3")
+  const start = {
+    audio,play
+  }
   return (       
     <div>
       <header className='App-header'/> 
         <p>
-        DONT <code>BE SHY </code>BUY  
+        DONT <code>BE SHY </code>BUY  <button onClick={start}/>
         <p>      </p>
         </p>
         <image src="/one.jpg" alt=""/>
